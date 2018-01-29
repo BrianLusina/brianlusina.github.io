a6a376b67fd8f55f3e1e334fd6c963ae3bac9989
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-01-29 23:53:27 +0300

    FIX: gatsby integration with redux

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
 import configureStore from "./src/store/configureStore";
+import { Router } from "react-router-dom"
 
 const store = configureStore();
 
-exports.wrapRootComponent = Root => {
-  return () => (
+exports.replaceRouterComponent = ({ history }) => {
+  const store = configureStore()
+
+  const ConnectedRouterWrapper = ({ children }) => (
     <Provider store={store}>
-      Root
+      <Router history={history}>{children}</Router>
     </Provider>
-  );
-};
+  )
+
+  return ConnectedRouterWrapper
+}
---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { renderToString } from "react-dom/server";
+import configureStore from "./src/store/configureStore";
+
+exports.replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+  const store = configureStore();
+
+  replaceBodyHTMLString(
+    renderToString(<Provider store={store}>{bodyComponent}</Provider>)
+  );
+};
