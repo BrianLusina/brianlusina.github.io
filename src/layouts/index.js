2df1573484b01e8a09d9c205b8d718b9d9b0a6a7
Brian Lusina <12752833+BrianLusina@users.noreply.github.com>
2018-02-12 00:29:43 +0300

    Create Social Icon component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,53 +3,53 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 //import AppError from "../components/errors/AppError";
-import Header from "../components/common/Header";
+import Header from "../components/Header";
 import SideBar from "../containers/sidebar/Sidebar";
 import "../styles/scss/main.scss";
 
 const TemplateWrapper = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: "description", content: "LJournal is a simple blog" },
-        {
-          name: "keywords",
-          content: "data, Lusina, Brian Lusina, code, bugs, algorithms"
-        }
-      ]}
-    />
-    <div id="wrapper">
-      <div id="main">
-        <div className="inner">
-          <Header />
-          {children()}
-        </div>
-      </div>
-      <SideBar />
-    </div>
-  </div>
+	<div>
+		<Helmet
+			title={data.site.siteMetadata.title}
+			meta={[
+				{ name: "description", content: "LJournal is a simple blog" },
+				{
+					name: "keywords",
+					content: "data, Lusina, Brian Lusina, code, bugs, algorithms"
+				}
+			]}
+		/>
+		<div id="wrapper">
+			<div id="main">
+				<div className="inner">
+					<Header />
+					{children()}
+				</div>
+			</div>
+			<SideBar />
+		</div>
+	</div>
 );
 
 /**
  * Prop Type validation
  */
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-  data: PropTypes.object
+	children: PropTypes.func,
+	data: PropTypes.object
 };
 
 /**
  * GraphQl query
  */
 export const query = graphql`
-  query RootLayoutQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
+	query RootLayoutQuery {
+		site {
+			siteMetadata {
+				title
+			}
+		}
+	}
 `;
 
 export default TemplateWrapper;
