9571162ec5a433aa99342ff6a3da42551a6b02bd
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2018-08-20 23:56:41 +0300

    create about page and data

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,15 @@ module.exports = {
 			options: {
 				name: "posts",
 				path: `${__dirname}/src/posts/`
-			},
+			}
+		},
+		"gatsby-transformer-json",
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: "data",
+				path: `${__dirname}/src/data`
+			}
 		}
 	],
 };
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,72 @@
 import React from 'react';
 import MainLayoutWrapper from '../layouts/MainLayout';
+import { graphql, withPrefix } from 'gatsby';
+import { string, shape,arrayOf} from "prop-types";
 
-
-const AboutPage = () => {
+const AboutPage = ({ data: { allAuthorsJson: { edges: authors } }}) => {
 	return (
 		<MainLayoutWrapper displaySidebar={false} pageTitle={"About"}>
-			<div />
+			<section>
+				<h3>About</h3>
+				<h4>About LJournal</h4>
+				{
+					authors.map(({ node: {id, name, web, email, bio, avatar_, link, twitter, google: { plus } }}) => {
+						return (
+							<p key={id}>
+								<span className="image left">
+									<img src={withPrefix(`images/authors/${avatar_}`)} alt={name}/>
+								</span>
+								{bio}
+							</p>)
+					})
+				}
+			</section>
 		</MainLayoutWrapper>
 	)
 }
+
+AboutPage.propTypes = {
+	data: shape({
+		allAuthorsJson: shape({
+			edges: arrayOf(shape({
+				node: shape({
+					id: string,
+					name: string,
+					web: string,
+					email: string,
+					bio: string,
+					avatar_: string,
+					link: string,
+					twitter: string,
+					google: shape({
+						plus: string
+					})	
+				})
+			}))
+		})
+	})
+}
+
+export const query = graphql`
+	query AboutPageQuery {
+		allAuthorsJson {
+			edges {
+				node {
+					id
+					name
+					web
+					email
+					bio
+					avatar_
+					link
+					twitter
+					google {
+						plus
+					}
+				}
+			}
+		}
+	}
+`
 
 export default AboutPage;
