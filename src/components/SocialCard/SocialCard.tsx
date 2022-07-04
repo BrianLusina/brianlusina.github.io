11308b8338cbea76dd6c1216d40ecba0af81eb55
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-07-04 20:41:36 +0300

    feat(component): social card
    Fetch social information from CMS

--- src/components/SocialCard/SocialCard.tsx
+++ src/components/SocialCard/SocialCard.tsx
@@ -1,13 +1,29 @@
 import { FunctionComponent } from 'react';
-import { SocialCardProps } from './SocialCard.types';
+import { useQuery } from '@apollo/client';
+import { GET_SOCIAL_INFO_QUERY } from '@graphQl/queries';
+import { captureException, captureScope, Levels } from '@monitoring';
 import SocialCardItem from './SocialCardItem';
 
-const SocialCard: FunctionComponent<SocialCardProps> = ({ items }) => {
+const SocialCard: FunctionComponent = () => {
+  const { loading, error, data } = useQuery<SocialsData>(GET_SOCIAL_INFO_QUERY);
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) {
+    captureException(
+      error,
+      captureScope({ type: 'component', data: { component: 'Blurb' } }, Levels.Error),
+    );
+    return <p>Yikes! Something terrible has happened. Looking into this :)</p>;
+  }
+
+  const socialItems = data ? data.socialCollection.items : [];
+
   return (
     <ul className="icons">
-      {items.map(({ iconName, label, link }) => (
-        <li key={label}>
-          <SocialCardItem iconName={iconName} link={link} label={label} />
+      {socialItems.map(({ name, link }) => (
+        <li key={name}>
+          <SocialCardItem iconName={name.toLocaleLowerCase()} link={link} label={name} />
         </li>
       ))}
     </ul>
