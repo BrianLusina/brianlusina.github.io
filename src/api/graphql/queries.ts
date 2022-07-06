016e1ff8ddb26258105421bc78450658ca075075
BrianLusina <12752833+BrianLusina@users.noreply.github.com>
2022-07-06 18:21:30 +0300

    feat(hooks): hooks to fetch data

--- src/api/graphql/queries.ts
+++ src/api/graphql/queries.ts
@@ -13,14 +13,14 @@ export const GET_SOCIAL_INFO_QUERY = gql`
 
 export const GET_BLURBS_QUERY = gql`
   query GetAllBlurbs {
-	  blurbCollection {
-		  items {
-			  description 
-			  image {
-				  url
-			  }
-			  title
-		  }
-	  }
+    blurbCollection {
+      items {
+        description
+        image {
+          url
+        }
+        title
+      }
+    }
   }
 `;
